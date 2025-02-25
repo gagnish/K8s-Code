925c61a1ac8ab9c14269e4fa4604e1c2a8f0eb9a
Nidhi Gupta <nidhi@Nidhis-MacBook-Air.local>
2025-02-25 09:23:34 -0500

    monitoring

--- terraform/eks_nodegroup/providers.tf
+++ terraform/eks_nodegroup/providers.tf
@@ -8,6 +8,10 @@ terraform {
       source = "hashicorp/helm"
       version = ">=2.11.0"  # Use the latest version available
     }
+    kubectl = {
+      source = "bnu0/kubectl"
+      version = "0.27.0"
+    }
   }
 
   backend "s3" {
